afebd00b5c8a5de5aba6b765e8651f3830c47b0e
Bushra Anwar <bushraanwar067@gmail.com>
2026-05-11 22:14:25 +0500

    Update API endpoint for data retrieval

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   }, []);
 
   async function reloadData() {
-    const response = await axios.get("/api/lists");
+    const response = await axios.get("https://to-do-list-backend-dusky.vercel.app/api/lists");
     const data = await response.data;
     setListSummaries(data);
   }
