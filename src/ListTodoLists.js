5822e774d2edc02a91af3bc9e78f2d073da3a468
Bushra Anwar <bushraanwar067@gmail.com>
2026-05-12 21:42:24 +0500

    Refactor ListToDoLists component and fix loading state

--- src/ListTodoLists.js
+++ src/ListTodoLists.js
@@ -1,96 +1,5 @@
-// import "./ListTodoLists.css";
-// import { useRef } from "react";
-// import { BiSolidTrash } from "react-icons/bi";
-
-// function ListToDoLists({
-//     listSummaries,
-//     handleSelectList,
-//     handleNewToDoList,
-//     handleDeleteToDoList,
-// }) {
-//     const labelRef = useRef();
-
-//     if (listSummaries === null) {
-//         return <div className="ListToDoLists Loading">⏳Loading to-do lists ...</div>;
-//     }else if (listSummaries.length === 0) {
-//         return (
-//             <div className="ListToDoLists"> 
-//             <div className="box"> 
-//             <label>
-//                 New To-Do List:&nbsp;
-//                 <input id={labelRef} type="text" />
-//             </label>
-//             <button 
-//             onClick={() =>
-//             handleNewToDoList(document.getElementById(labelRef).value)
-//             } 
-//             > 
-//             New
-//             </button>
-//             </div>
-//             <p>There are no to-do lists!</p>
-//         </div>
-//        );
-//     }
-//     return (
-//         <div className="ListToDoLists">
-//             <h1>All To-Do Lists</h1> 
-//             <div className="box"> 
-//             <label>
-//                 New To-Do List:&nbsp;
-//                 <input id={labelRef} type="text" />
-//             </label>
-//             <button 
-//             onClick={() =>
-//             handleNewToDoList(document.getElementById(labelRef).value)
-//             } 
-//             > 
-//             New
-//             </button>
-//           </div>
-//             {listSummaries.map((summary) => {
-//             return (
-//                 <div
-//                     key={summary.id}
-//                     className="summary"
-//                     onClick={() => handleSelectList(summary.id)}
-//                     >
-//                         <span className="name">{summary.name} </span>
-//                         <span className="count">({summary.item_count} items)</span>
-//                         <span className="flex"></span>
-//                         <span 
-//                         className="trash"
-//                         onClick={(evt) => {
-//                             evt.stopPropagation();
-//                             handleDeleteToDoList(summary.id);
-//                         }}
-//                         >
-//                          <BiSolidTrash />
-//                      </span>
-//                 </div>
-//              );
-//         })}
-//     </div> 
-//   );
-// }
-
-// export default ListToDoLists;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import "./ListTodoLists.css";
-import { useState } from "react";
+import { useRef } from "react";
 import { BiSolidTrash } from "react-icons/bi";
 
 function ListToDoLists({
@@ -99,72 +8,85 @@ function ListToDoLists({
     handleNewToDoList,
     handleDeleteToDoList,
 }) {
-    const [newListName, setNewListName] = useState("");
+    const labelRef = useRef();
 
-    if (!Array.isArray(listSummaries)) {
+    if (listSummaries === null) {
+        return <div className="ListToDoLists Loading">⏳Loading to-do lists ...</div>;
+    }else if (listSummaries.length === 0) {
         return (
-            <div className="ListToDoLists Loading">
-                ⏳ Loading to-do lists ...
+            <div className="ListToDoLists"> 
+            <div className="box"> 
+            <label>
+                New To-Do List:&nbsp;
+                <input id={labelRef} type="text" />
+            </label>
+            <button 
+            onClick={() =>
+            handleNewToDoList(document.getElementById(labelRef).value)
+            } 
+            > 
+            New
+            </button>
             </div>
-        );
+            <p>There are no to-do lists!</p>
+        </div>
+       );
     }
-
     return (
         <div className="ListToDoLists">
-            <h1>All To-Do Lists</h1>
-
-            <div className="box">
-                <label>
-                    New To-Do List:&nbsp;
-                    <input
-                        type="text"
-                        value={newListName}
-                        onChange={(e) => setNewListName(e.target.value)}
-                    />
-                </label>
-                <button
-                    onClick={() => {
-                        if (newListName.trim() !== "") {
-                            handleNewToDoList(newListName);
-                            setNewListName("");
-                        }
-                    }}
-                >
-                    New
-                </button>
-            </div>
-
-            {listSummaries.length === 0 ? (
-                <p>There are no to-do lists!</p>
-            ) : (
-                listSummaries.map((summary) => (
-                    <div
-                        key={summary.id}
-                        className="summary"
-                        onClick={() => handleSelectList(summary.id)}
+            <h1>All To-Do Lists</h1> 
+            <div className="box"> 
+            <label>
+                New To-Do List:&nbsp;
+                <input id={labelRef} type="text" />
+            </label>
+            <button 
+            onClick={() =>
+            handleNewToDoList(document.getElementById(labelRef).value)
+            } 
+            > 
+            New
+            </button>
+          </div>
+            {listSummaries.map((summary) => {
+            return (
+                <div
+                    key={summary.id}
+                    className="summary"
+                    onClick={() => handleSelectList(summary.id)}
                     >
-                        <span className="name">{summary.name}</span>
-                        <span className="count">
-                            ({summary.item_count} items)
-                        </span>
+                        <span className="name">{summary.name} </span>
+                        <span className="count">({summary.item_count} items)</span>
                         <span className="flex"></span>
-                        <span
-                            className="trash"
-                            onClick={(evt) => {
-                                evt.stopPropagation();
-                                handleDeleteToDoList(summary.id);
-                            }}
+                        <span 
+                        className="trash"
+                        onClick={(evt) => {
+                            evt.stopPropagation();
+                            handleDeleteToDoList(summary.id);
+                        }}
                         >
-                            <BiSolidTrash />
-                        </span>
-                    </div>
-                ))
-            )}
-        </div>
-    );
+                         <BiSolidTrash />
+                     </span>
+                </div>
+             );
+        })}
+    </div> 
+  );
 }
 
 export default ListToDoLists;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
